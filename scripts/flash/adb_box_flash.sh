c6c70f962bace8abbc65e6a3f364ab48113a7124
Audioniek <audioniek@xs4all.nl>
2019-07-17 00:59:04 +0200

    Fix adb_box flash problems

--- scripts/flash/adb_box_flash.sh
+++ scripts/flash/adb_box_flash.sh
@@ -6,12 +6,18 @@
 # " with Freebox or compatible bootloader"
 #
 # "Author: Schischu/Audioniek"
-# "Date: 05-30-2019"   Last change 05-30-2019
+# "Date: 05-30-2019"   Last change 07-17-2019
 #
 # "-----------------------------------------------------------------------"
 # "It is assumed that an image was already built prior to executing this"
 # "script!"
 # "-----------------------------------------------------------------------"
+#
+# Date     Who          Description
+# 20190717 Audioniek    Fix several errors.
+#
+# -----------------------------------------------------------------------
+
 #
 
 # Set up the variables
@@ -51,162 +57,188 @@ elif [ ! -d $OUTDIR ]; then
   mkdir -p $OUTDIR
 fi
 
-echo -n " - Preparing kernel file..."
-cp $TMPKERNELDIR/uImage $TMPDIR/uImage
-KERNELCRC=`$MKCRC32 $TMPDIR/uImage`
-echo " done."
+if [ ! "$FIMAGE" == "image" ]; then
+  echo -n " - Preparing kernel file..."
+  cp $TMPKERNELDIR/uImage $TMPDIR/uImage
+  echo " done."
+  echo -n " - Calculating CRC32..."
+  KERNELCRC=`$MKCRC32 $TMPDIR/uImage`
+  echo " done ($KERNELCRC)."
 
-echo -n " - Checking kernel size..."
-SIZEK=`stat $TMPDIR/uImage -t --format %s`
-SIZEKD=`printf "%d" $SIZEK`
-SIZEKH=`printf "%08X" $SIZEK`
-KERNELSIZE=`printf "%x" $SIZEK`
-if [[ $SIZEKD > "4194304" ]]; then
-  echo
-  echo -e "\033[01;31m"
-  echo "-- ERROR! -------------------------------------------------------------"
-  echo
-  echo " KERNEL TOO BIG: 0x$SIZEKH instead of max. 0x00400000 bytes." > /dev/stderr
-  echo " Exiting..."
-  echo
-  echo "-----------------------------------------------------------------------"
-  echo -e "\033[00m"
-  exit
-else
-  echo " OK: $SIZEKD (0x$SIZEKH, max. 0x00400000) bytes."
+  echo -n " - Checking kernel size..."
+  SIZEK=`stat $TMPDIR/uImage -t --format %s`
+  SIZEKD=`printf "%d" $SIZEK`
+  SIZEKH=`printf "%08X" $SIZEK`
+  KERNELSIZE=`printf "%x" $SIZEK`
+  if [[ $SIZEKD > "4194304" ]]; then
+    echo
+    echo -e "\033[01;31m"
+    echo "-- ERROR! -------------------------------------------------------------"
+    echo
+    echo " KERNEL TOO BIG: 0x$SIZEKH instead of max. 0x00400000 bytes." > /dev/stderr
+    echo " Exiting..."
+    echo
+    echo "-----------------------------------------------------------------------"
+    echo -e "\033[00m"
+    exit
+  else
+    echo " OK: $SIZEKD (0x$SIZEKH, max. 0x00400000) bytes."
+  fi
 fi
 
 if [ ! "$FIMAGE" == "kernel" ]; then
   echo -n " - Preparing JFFS root file system..."
   # Create a jffs2 partition for the complete root
-  $MKFSJFFS2 -qUfl -e 0x20000 -r $TMPROOTDIR -o $TMPDIR/root.bin
-  $SUMTOOL -p -l -e 0x20000 -i $TMPDIR/root.bin -o $TMPDIR/root.sum > /dev/null
-  # Padding the root up maximum size is required to force JFFS2 to find
-  # only erased flash blocks on the initial kernel run.
-  $PAD 0x3C00000 $TMPDIR/root.sum $TMPDIR/root.pad
+  $MKFSJFFS2 -qUfl -e 0x4000 -c 16 -r $TMPROOTDIR -o $TMPDIR/root.bin
+  $SUMTOOL -p -l -e 0x10000 -i $TMPDIR/root.bin -o $TMPDIR/root.sum > /dev/null
+#  # Pad rootfs upto the next 64 kbyte; this is required to force JFFS2 to find
+#  # only erased flash blocks on the initial kernel run.
+#  echo -e "\nSIZE_SUM = $SIZE_SUM"
+#  SIZE_SUMD=`printf "%d" $SIZE_SUM`
+#  echo "SIZE_SUMD = $SIZE_SUMD"
+#  SIZE_SUM_REMAINDER=$(expr $SIZE_SUMD % 65536)
+#  echo "SIZE_SUM_REMAINDER = $SIZE_SUM_REMAINDER"
+#  SIZE_SUM_PAD=$(expr $SIZE_SUMD + $SIZE_SUM_REMAINDER)
+#  echo "SIZE_SUM_PAD = $SIZE_SUM_PAD"
+#  SIZE_SUM_PADH=`printf "%x" $SIZE_SUM_PAD`
+#  echo "SIZE_SUM_PADH = 0x$SIZE_SUM_PADH"
+#  $PAD 0x$SIZE_SUM_PADH $TMPDIR/root.sum $TMPDIR/root.pad
+#  $PAD 0x$SIZE_SUM_PADH $TMPDIR/root.sum $TMPDIR/root.pad
   echo " done."
+  echo -n " - Calculating CRC32..."
+  ROOTCRC=`$MKCRC32 $TMPDIR/root.sum`
+  echo " done ($ROOTCRC)."
+
+  echo -n " - Checking root size..."
+  SIZE=`stat $TMPDIR/root.sum -t --format %s`
+  SIZEH=`printf "%08X" $SIZE`
+  SIZED=`printf "%d" $SIZE`
+  ROOTSIZE=`printf "%x" $SIZE`
+  if [[ $ROOTSIZE > "62914560" ]]; then
+    echo -e "\033[01;31m"
+    echo
+    echo -e "\033[01;31m"
+    echo "-- ERROR! -------------------------------------------------------------"
+    echo
+    echo " ROOTFS TOO BIG: 0x$SIZEH instead of max. 0x03C00000 bytes." > /dev/stderr
+    echo " Exiting..."
+    echo
+    echo "-----------------------------------------------------------------------"
+    echo -e "\033[00m"
+    exit
+  else
+    echo " OK: $SIZED (0x$SIZEH, max. 0x03C00000) bytes."
+  fi
 fi
-echo -n " - Checking root size..."
-SIZE=`stat $TMPDIR/root.bin -t --format %s`
-SIZEH=`printf "%08X" $SIZE`
-SIZED=`printf "%d" $SIZE`
-ROOTSIZE=`printf "%x" $SIZE`
-if [[ $ROOTSIZE > "62914560" ]]; then
-  echo -e "\033[01;31m"
-  echo
-  echo -e "\033[01;31m"
-  echo "-- ERROR! -------------------------------------------------------------"
-  echo
-  echo " ROOT TOO BIG: 0x$SIZEH instead of max. 0x03C00000 bytes." > /dev/stderr
-  echo " Exiting..."
-  echo
-  echo "-----------------------------------------------------------------------"
-  echo -e "\033[00m"
-  exit
-else
-  echo " OK: $ROOTSIZE (0x$SIZEH, max. 0x03C00000) bytes."
-fi
-ROOTCRC=`$MKCRC32 $TMPDIR/root.pad`
 # note: rootfs is not converted to an u-boot image
 
-# TODO: integrate uboot and depencies for kernel-only/image-only
+# TODO: integrate uboot
 echo -n " - Creating update.img file..."
-cd $OUTDIR
+cd $TMPDIR
 echo "if crc32; then" > update.txt
 echo >> update.txt
-echo "vfd L--1" >> update.txt
-echo >> update.txt
-echo "if fatload usb 0:1 84010000 kernel.img; then" >> update.txt
-echo -e "\tif crc32 -v 84010000 $KERNELSIZE $KERNELCRC; then" >> update.txt
-echo -e "\t\tvfd S--1" >> update.txt
-echo -e "\t\tnand unlock" >> update.txt
-echo -e "\t\tnand erase 3c00000 400000" >> update.txt
-echo -e "\t\tnand write.i 84010000 3c00000 $KERNELSIZE" >> update.txt
-echo -e "\t\tnand read.i 84010000 3c00000 $KERNELSIZE" >> update.txt
-echo -e "\t\tif crc32 -v 84010000 $KERNELSIZE $KERNELCRC; then" >> update.txt
-echo -e "\t\t\techo" >> update.txt
-echo -e "\t\telse" >> update.txt
-echo -e "\t\t\tvfd CS-1" >> update.txt
-echo -e "\t\t\tstop" >> update.txt
-echo -e "\t\tfi" >> update.txt
-echo -e "\telse" >> update.txt
-echo -e "\t\tvfd CR-1" >> update.txt
-echo -e "\t\tstop" >> update.txt
-echo -e "\tfi" >> update.txt
-echo -e "else" >> update.txt
-echo -e "\tvfd LE-1" >> update.txt
-echo -e "\tstop" >> update.txt
-echo "fi" >> update.txt
-echo >> update.txt
-echo "vfd L--2" >> update.txt
-echo "if fatload usb 0:1 84010000 rootfs.img; then" >> update.txt
-echo -e "\tif crc32 -v 84010000 $ROOTSIZE $ROOTCRC; then" >> update.txt
-echo -e "\t\tvfd S--2" >> update.txt
-echo -e "\t\tnand unlock" >> update.txt
-echo -e "\t\tnand erase 0 3c00000" >> update.txt
-echo -e "\t\tnand write.jffs2 84010000 0 $ROOTSIZE" >> update.txt
-echo -e "\t\tnand read.jffs2 84010000 0 $ROOTSIZE" >> update.txt
-echo -e "\t\tif crc32 -v 84010000 $ROOTSIZE $ROOTCRC; then" >> update.txt
-echo -e '\t\t\techo ""' >> update.txt
-echo -e "\t\telse" >> update.txt
-echo -e "\t\t\tvfd CS-2" >> update.txt
-echo -e "\t\t\tstop" >> update.txt
-echo -e "\t\tfi" >> update.txt
-echo -e "\telse" >> update.txt
-echo -e "\t\tvfd CR-2" >> update.txt
-echo -e "\t\tstop" >> update.txt
-echo -e "\tfi" >> update.txt
-echo "else" >> update.txt
-echo -e "\tvfd LE-2" >> update.txt
-echo -e "\tstop" >> update.txt
-echo "fi" >> update.txt
-echo >> update.txt
+if [ -e uImage ]; then
+  echo "vfd L--1" >> update.txt
+  echo >> update.txt
+  echo "if fatload usb 0:1 84010000 kernel.img; then" >> update.txt
+  echo -e "\tif crc32 -v 84010000 $KERNELSIZE $KERNELCRC,; then" >> update.txt
+  echo -e "\t\tvfd S--1" >> update.txt
+  echo -e "\t\tnand unlock" >> update.txt
+  echo -e "\t\tnand erase 3c00000 400000" >> update.txt
+  echo -e "\t\tnand write.i 84010000 3c00000 $KERNELSIZE" >> update.txt
+  echo -e "\t\tnand read.i 84010000 3c00000 $KERNELSIZE" >> update.txt
+  echo -e "\t\tif crc32 -v 84010000 $KERNELSIZE $KERNELCRC,; then" >> update.txt
+  echo -e "\t\t\techo" >> update.txt
+  echo -e "\t\telse" >> update.txt
+  echo -e "\t\t\tvfd CS-1" >> update.txt
+  echo -e "\t\t\tstop" >> update.txt
+  echo -e "\t\tfi" >> update.txt
+  echo -e "\telse" >> update.txt
+  echo -e "\t\tvfd CR-1" >> update.txt
+  echo -e "\t\tstop" >> update.txt
+  echo -e "\tfi" >> update.txt
+  echo -e "else" >> update.txt
+  echo -e "\tvfd LE-1" >> update.txt
+  echo -e "\tstop" >> update.txt
+  echo "fi" >> update.txt
+  echo >> update.txt
+fi
+if [ -e root.sum ]; then
+  echo "vfd L--2" >> update.txt
+  echo "if fatload usb 0:1 84010000 rootfs.img; then" >> update.txt
+  echo -e "\tif crc32 -v 84010000 $ROOTSIZE $ROOTCRC,; then" >> update.txt
+  echo -e "\t\tvfd S--2" >> update.txt
+  echo -e "\t\tnand unlock" >> update.txt
+  echo -e "\t\tnand erase 0 3c00000" >> update.txt
+  echo -e "\t\tnand write.jffs2 84010000 0 $ROOTSIZE" >> update.txt
+  echo -e "\t\tnand read.jffs2 84010000 0 $ROOTSIZE" >> update.txt
+  echo -e "\t\tif crc32 -v 84010000 $ROOTSIZE $ROOTCRC,; then" >> update.txt
+  echo -e '\t\t\techo ""' >> update.txt
+  echo -e "\t\telse" >> update.txt
+  echo -e "\t\t\tvfd CS-2" >> update.txt
+  echo -e "\t\t\tstop" >> update.txt
+  echo -e "\t\tfi" >> update.txt
+  echo -e "\telse" >> update.txt
+  echo -e "\t\tvfd CR-2" >> update.txt
+  echo -e "\t\tstop" >> update.txt
+  echo -e "\tfi" >> update.txt
+  echo "else" >> update.txt
+  echo -e "\tvfd LE-2" >> update.txt
+  echo -e "\tstop" >> update.txt
+  echo "fi" >> update.txt
+  echo >> update.txt
+fi
 echo "vfd -OK-" >> update.txt
 echo "stop" >> update.txt
 echo >> update.txt
 echo "else" >> update.txt
 echo >> update.txt
-echo 'echo "No crc32 program found; do not check u-boot CRC32"' >> update.txt
+echo 'echo "No crc32 program found; do not check CRC32"' >> update.txt
 echo >> update.txt
-echo "vfd L--1" >> update.txt
-echo "if fatload usb 0:1 84010000 kernel.img; then" >> update.txt
-echo -e "\tvfd S--1" >> update.txt
-echo -e "\tnand unlock" >> update.txt
-echo -e "\tnand erase 3c00000 400000" >> update.txt
-echo -e "\tnand write.i 84010000 3c00000 $KERNELSIZE" >> update.txt
-echo "else" >> update.txt
-echo -e "\tvfd LE-1" >> update.txt
-echo -e "\tstop" >> update.txt
-echo "fi" >> update.txt
-echo >> update.txt
-echo "vfd L--2" >> update.txt
-echo "if fatload usb 0:1 84010000 rootfs.img; then" >> update.txt
-echo -e "\tvfd S--2" >> update.txt
-echo -e "\tnand unlock" >> update.txt
-echo -e "\tnand erase 0 3c00000" >> update.txt
-echo -e "\tnand write.jffs2 84010000 0 3174000" >> update.txt
-echo "else" >> update.txt
-echo -e "\tvfd LE-2" >> update.txt
-echo -e "\tstop" >> update.txt
-echo "fi" >> update.txt
-echo >> update.txt
+if [ -e uImage ]; then
+  echo "vfd L--1" >> update.txt
+  echo "if fatload usb 0:1 84010000 kernel.img; then" >> update.txt
+  echo -e "\tvfd S--1" >> update.txt
+  echo -e "\tnand unlock" >> update.txt
+  echo -e "\tnand erase 3c00000 400000" >> update.txt
+  echo -e "\tnand write.i 84010000 3c00000 $KERNELSIZE" >> update.txt
+  echo "else" >> update.txt
+  echo -e "\tvfd LE-1" >> update.txt
+  echo -e "\tstop" >> update.txt
+  echo "fi" >> update.txt
+  echo >> update.txt
+fi
+if [ -e root.sum ]; then
+  echo "vfd L--2" >> update.txt
+  echo "if fatload usb 0:1 84010000 rootfs.img; then" >> update.txt
+  echo -e "\tvfd S--2" >> update.txt
+  echo -e "\tnand unlock" >> update.txt
+  echo -e "\tnand erase 0 3c00000" >> update.txt
+  echo -e "\tnand write.jffs2 84010000 0 $ROOTSIZE" >> update.txt
+  echo "else" >> update.txt
+  echo -e "\tvfd LE-2" >> update.txt
+  echo -e "\tstop" >> update.txt
+  echo "fi" >> update.txt
+  echo >> update.txt
+fi
 echo "vfd -OK-" >> update.txt
 echo "stop" >> update.txt
 echo >> update.txt
 echo "fi" >> update.txt
-# update.img
-mkimage -T script -C none -n update -d update.txt $OUTDIR/$OUTFILEU > /dev/null
 echo " done."
 cd $CURDIR
 
 echo -n " - Creating flash files and MD5s..."
+# update.img
+$MKIMAGE -T script -C none -n update -d $TMPDIR/update.txt $OUTDIR/$OUTFILEU > /dev/null
 # kernel.img
 if [ -e $TMPDIR/uImage ]; then
-  mkimage -A sh -T kernel -C gzip -a 0x84001000 -e 0x84002000 -n Linux-2.6.32.71_stm24_0217 -d $TMPDIR/uImage $OUTDIR/$OUTFILEK > /dev/null
+#  $MKIMAGE -A sh -T kernel -C gzip -a 0x84001000 -e 0x84002000 -n Linux-2.6.32.71_stm24_0217 -d $TMPDIR/uImage $OUTDIR/$OUTFILEK > /dev/null
+  cp $TMPDIR/uImage $OUTDIR/$OUTFILEK
 fi
 # rootfs.img
-if [ -e $TMPDIR/root.pad ]; then
-  cp $TMPDIR/root.pad $OUTDIR/$OUTFILER
+if [ -e $TMPDIR/root.sum ]; then
+  cp $TMPDIR/root.sum $OUTDIR/$OUTFILER
 fi
 
 # Create MD5 files
@@ -234,7 +266,7 @@ fi
 cd $CURDIR
 echo " done."
 
-if [ -e $OUTDIR/$OUTFILE ]; then
+if [ -e $OUTDIR/$OUTZIPFILE ]; then
   echo -e "\033[01;32m"
   echo "-- Instructions -------------------------------------------------------"
   echo
